ad2f88c1eb9ac5580ab8b2302f11ecb7ab199fc4
ma-he-sh <mra@vocalmeet.com>
2023-03-02 00:05:44 -0500

    --working prototype--

--- server/readme.md
+++ server/readme.md
@@ -4,3 +4,6 @@
 yarn start
 yarn build
 ```
+
+// use a token a refer to previous message
+// use a uuid for the current session (no need for database)
--- server/server.py
+++ server/server.py
@@ -7,7 +7,14 @@ from modules.db.db import DB
 from modules.db.sessions import SessionHandler
 import json
 
-from flask_socketio import SocketIO, join_room, leave_room, send, emit
+import validators
+import requests
+import openai
+
+from bs4 import BeautifulSoup
+import re
+
+from modules.model import ModelGPT
 
 # initialize database
 webdb = DB( config.DB_CONFIG )
@@ -15,7 +22,6 @@ webdb.db_create()
 webdb.close()
 
 app = Flask(__name__, static_url_path='/static', static_folder='build/static', template_folder='build')
-socket = SocketIO(app)
 
 def send_response(is_success=False, payload=None):
     if payload is None:
@@ -29,8 +35,101 @@ def send_response(is_success=False, payload=None):
 def root():
     sessionHandler = SessionHandler(config.DB_CONFIG)
     maps = sessionHandler.get_all()
+
+    return render_template('index.html', mapdata=maps, curr_session=uuid.uuid4())
+
+@app.route('/get_prompt', methods=['POST'])
+def get_prompt():
+    payload = request.json
+
+    api_key = payload['api_key']
+    site_url= payload['site_url']
+    prompt  = payload['prompt']
+
+    if api_key is None or len(api_key) == 0:
+        resp = {
+            'success': False,
+            'response': 'API KEY required'
+        }
+        return send_response( False, resp )
+
+    if site_url is None or len(site_url) == 0:
+        resp = {
+            'success': False,
+            'response': 'SITE URL required'
+        }
+        return send_response( False, resp )
+
+    if not validators.url( site_url ):
+        resp = {
+            'success': False,
+            'response': 'SITE URL invalid'
+        }
+        return send_response( False, resp )
+
+    if prompt is None or len(prompt) == 0:
+        resp = {
+            'success': False,
+            'response': 'PROMPT required'
+        }
+        return send_response( False, resp )
+
+    # validate if token is valid
+    validToken = True
+    openai.api_key = api_key
+    try: 
+        models = openai.Model.list()
+    except:
+        print('Error')
+        validToken=False
+
+    if not validToken:
+        resp = {
+            'success': False,
+            'response': 'Invalid API-KEY'
+        }
+        return send_response( False, resp )
     
-    return render_template('index.html', mapdata=maps)
+    page = requests.get(site_url)
+
+    pageContent = ""
+    if page:
+        # parse the HTML document using BeautifulSoup
+        soup = BeautifulSoup(page.text, "html.parser")
+
+        # remove all script and style tags from the HTML document
+        for script in soup(["script", "style", "a"]):
+            script.decompose()
+
+        # extract the text from the HTML document
+        text = soup.get_text()
+
+        # split the text into lines and then into chunks
+        chunks = (phrase.strip() for line in text.splitlines() for phrase in line.split("  "))
+
+        # join the non-empty chunks into a string
+        text = '\n'.join(chunk for chunk in chunks if chunk)
+        text = ''.join(filter(lambda x: x.isprintable(), text))
+
+        pageContent = text
+    
+    if not pageContent:
+        resp = {
+            'success': False,
+            'response': 'Failed to request page content, please try another url'
+        }
+        return send_response( False, resp )
+
+    model = ModelGPT( "", api_key )
+    response = model.get_message( prompt, pageContent )
+
+    resp = {
+        'success': True,
+        'prompt' : prompt,
+        'response': response
+    }
+
+    return send_response(True, resp)
 
 @app.errorhandler(404)
 def error_404(e):
@@ -41,5 +140,5 @@ def error_500(e):
     return '500'
 
 if __name__ == '__main__':
-    socket.run(app, debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
